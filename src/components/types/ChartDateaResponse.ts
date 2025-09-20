596064d97412cbbf6f021db4a44178cea219c9f1
CauevSilv <127447047+CauevSilv@users.noreply.github.com>
2025-09-19 23:58:51 -0300

    ✨ (feat): add ticketsByProduct interface objects

--- src/components/types/ChartDateaResponse.ts
+++ src/components/types/ChartDateaResponse.ts
@@ -1,3 +1,6 @@
+import type { TicketsByProduct } from '@/components/types/ChartTypes/TicketsByProduct.ts'
+
 export interface ChartDate {
   recidivismRate: number;
+  ticketsCountGroupedByProduct : TicketsByProduct[];
 }
