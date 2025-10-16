266985983220caec6941a01404ce0000c81d6282
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-10-16 19:15:51 -0300

    ✨(feat): added insights route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DashboardView from '../views/DashboardView.vue'
+import InsightsView from '@/views/InsightsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'dashboard',
+      component: DashboardView,
+    },
+    {
+      path: '/insights',
+      name: 'insights',
+      component: InsightsView,
     },
   ],
 })
