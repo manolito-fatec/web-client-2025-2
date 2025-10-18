dd8eb3729b15a1ab7383538ee27abe116072a404
CaueVSilv <cauevsilva2002@gmail.com>
2025-10-18 19:21:49 +0000

    ✨ (feat): add login and email validation routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
+import AccountConfirmView from '@/views/AccountConfirmView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView,
     },
     {
-      path: '/login',
+      path: '/',
       name: 'login',
       component: LoginView,
+    },
+    {
+      path: '/verify-email/:token',
+      component: AccountConfirmView,
+      name: 'verify-email'
     }
   ],
 })
