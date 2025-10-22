c7e0aaf4163a04ac0c1e09a378a63be41cb8b62a
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-10-22 01:07:18 -0300

    🐛 (fix): client options property error

--- src/views/InsightsView.vue
+++ src/views/InsightsView.vue
@@ -125,7 +125,7 @@ const loadClientOptions = async () => {
     const companyOptions: FilterCompany[] = allCompanies.map(
       (company) => ({
         name: company.name,
-        code: company.id.toString(),
+        code: company.code
       }),
     )
 
