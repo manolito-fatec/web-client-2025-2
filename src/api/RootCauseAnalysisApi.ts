bff2dca7aad9a808d8f698ef44eb04a4e6cb9c92
CaueVSilv <cauevsilva2002@gmail.com>
2025-10-23 02:29:13 +0000

    🐛 (fix): api endpoint calls

--- src/api/RootCauseAnalysisApi.ts
+++ src/api/RootCauseAnalysisApi.ts
@@ -1,7 +1,7 @@
 import type { InsightsDataDto, RootCauseAnalysisData, RawParetoItem } from "@/types/RootCauseAnalysisResponse";
-import axios from "axios";
+import api from '@/api/axios/AxiosConfig.ts'
 
-const BASE_URL = 'http://localhost:8080/api/insights/data';
+const BASE_URL = 'api/insights/data';
 
 const transformParetoListToMap = (data: RawParetoItem[]): RootCauseAnalysisData => {
   const groupedData: RootCauseAnalysisData = {};
@@ -34,7 +34,7 @@ export async function getRootCauseAnalysis(companyId: string | null)
 
     const url = clientParam ? `${BASE_URL}?${clientParam}` : BASE_URL;
 
-    const response = await axios.get<InsightsDataDto>(url);
+    const response = await api.get<InsightsDataDto>(url);
 
     const rawList = response.data?.paretoInsightData || [];
 
